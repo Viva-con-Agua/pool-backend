a7283394c061443d924f63cac24ebec802ec154a
Dennis Kleber <dennis_kleber@mailbox.org>
2023-12-20 16:20:29 +0100

    add update ticker for eventstate in the case that an event has no income

--- dao/update_ticker.go
+++ dao/update_ticker.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"pool-backend/models"
 	"time"
 
 	"github.com/Viva-con-Agua/vcago/vmdb"
@@ -19,6 +20,7 @@ func UpdateTicker() {
 			select {
 			case <-ticker.C:
 				EventStateUpdateTicker()
+				EventStateNoIncome()
 			case <-quit:
 				ticker.Stop()
 				return
@@ -34,5 +36,23 @@ func EventStateUpdateTicker() {
 	update := bson.D{{Key: "event_state.state", Value: "finished"}}
 	if err := EventCollection.UpdateMany(context.Background(), filter.Bson(), vmdb.UpdateSet(update)); err != nil {
 		log.Print(err)
+	}
+}
+func EventStateNoIncome() {
+	filter := vmdb.NewFilter()
+	filter.EqualBool("no_income", "true")
+	filter.EqualString("event.event_state.state", "finished")
+	pipeline := models.TakingPipelineTicker().Match(filter.Bson()).Pipe
+	takings := []models.Taking{}
+	if err := TakingCollection.Aggregate(context.Background(), pipeline, takings); err != nil {
+		log.Print(err)
+	}
+	for i := range takings {
+		updateFilter := bson.D{{Key: "_id", Value: takings[i].Event.ID}}
+		update := bson.D{{Key: "event_state.state", Value: "closed"}}
+		if err := TakingCollection.UpdateOne(context.Background(), updateFilter, vmdb.UpdateSet(update), nil); err != nil {
+			log.Print(err)
+		}
+
 	}
 }
