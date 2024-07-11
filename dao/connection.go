41bf65f5b359520a232d6b252199f4749364985a
Dennis Kleber <dennis_kleber@mailbox.org>
2024-07-11 12:01:52 +0200

    set index

--- dao/connection.go
+++ dao/connection.go
@@ -119,9 +119,9 @@ func InitialDatabase() {
 	OrganizerCollection = Database.Collection(models.OrganizerCollection).CreateIndex("name", true)
 	EventCollection = Database.Collection(models.EventCollection)
 	SourceCollection = Database.Collection(models.SourceCollection)
-	TakingCollection = Database.Collection(models.TakingCollection)
+	TakingCollection = Database.Collection(models.TakingCollection).CreateIndex("crew_id", false)
 	DepositCollection = Database.Collection(models.DepositCollection)
-	DepositUnitCollection = Database.Collection(models.DepositUnitCollection).CreateMultiIndex(bson.D{{Key: "taking_id", Value: 1}, {Key: "deposit_id", Value: 1}}, true)
+	DepositUnitCollection = Database.Collection(models.DepositUnitCollection).CreateMultiIndex(bson.D{{Key: "taking_id", Value: 1}, {Key: "deposit_id", Value: 1}}, true).CreateIndex("taking_id", false).CreateIndex("deposit_id", false)
 
 	FSChunkCollection = Database.Collection(models.FSChunkCollection)
 	FSFilesCollection = Database.Collection(models.FSFilesCollection)
