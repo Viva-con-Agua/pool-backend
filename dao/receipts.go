385c0de628a879e98d1746a7e974bf66bd117472
Dennis Kleber <dennis_kleber@mailbox.org>
2024-09-11 13:28:13 +0200

    download all images as zip

--- dao/receipts.go
+++ dao/receipts.go
@@ -1,8 +1,12 @@
 package dao
 
 import (
+	"archive/zip"
+	"bytes"
 	"context"
+	"io"
 	"pool-backend/models"
+	"strconv"
 
 	"github.com/Viva-con-Agua/vcago/vmod"
 	"github.com/Viva-con-Agua/vcapool"
@@ -57,6 +61,42 @@ func ReceiptFileGetByID(
 	if result, err = Database.DownloadFile(id.ID); err != nil {
 		return
 	}
+	return
+}
+
+func ReceiptFileZipGetByID(
+	ctx context.Context,
+	id *vmod.IDParam,
+	token *vcapool.AccessToken,
+) (
+	result []byte,
+	err error,
+) {
+	if err = models.DepositPermission(token); err != nil {
+		return
+	}
+	deposit := new(models.Deposit)
+	filter := bson.D{{Key: "_id", Value: id.ID}}
+	if err = DepositCollection.AggregateOne(ctx, models.DepositPipeline().Match(filter).Pipe, deposit); err != nil {
+		return
+	}
+	buf := new(bytes.Buffer)
+	w := zip.NewWriter(buf)
+	for index, file := range deposit.Receipts {
+		var bbuffer []byte
+		if bbuffer, err = Database.DownloadFile(file.ID); err != nil {
+			return
+		}
+		var f io.Writer
+		if f, err = w.Create(deposit.ReasonForPayment + "_" + strconv.Itoa(index+1) + ".png"); err != nil {
+			return
+		}
+		if _, err = f.Write(bbuffer); err != nil {
+			return
+		}
+	}
+	err = w.Close()
+	result = buf.Bytes()
 	return
 }
 
--- handlers/token/receipts.go
+++ handlers/token/receipts.go
@@ -22,7 +22,9 @@ func (i *ReceiptFileHandler) Routes(group *echo.Group) {
 	group.Use(i.Context)
 	group.POST("", i.Upload, accessCookie)
 	group.GET("/:id", i.GetByID, accessCookie)
+	group.GET("/zip/:id", i.GetZipByID, accessCookie)
 	group.DELETE("/:id", i.DeleteByID, accessCookie)
+
 }
 
 func (i *ReceiptFileHandler) Upload(cc echo.Context) (err error) {
@@ -61,6 +63,23 @@ func (i *ReceiptFileHandler) GetByID(cc echo.Context) (err error) {
 		return
 	}
 	return c.Stream(http.StatusOK, "image/png", bytes.NewReader(result))
+}
+
+func (i *ReceiptFileHandler) GetZipByID(cc echo.Context) (err error) {
+	c := cc.(vcago.Context)
+	token := new(vcapool.AccessToken)
+	if err = c.AccessToken(token); err != nil {
+		return
+	}
+	body := new(vmod.IDParam)
+	if err = c.BindAndValidate(body); err != nil {
+		return
+	}
+	var result []byte
+	if result, err = dao.ReceiptFileZipGetByID(c.Ctx(), body, token); err != nil {
+		return
+	}
+	return c.Stream(http.StatusOK, "application/zip", bytes.NewReader(result))
 }
 
 func (i *ReceiptFileHandler) DeleteByID(cc echo.Context) (err error) {
