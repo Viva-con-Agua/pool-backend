3ffd4ada4c402161ce00b11082fabd6d5c59565b
Eike Broda <git@ebroda.de>
2025-03-11 13:37:56 +0100

    Re-run update_deposit_units (fixes #257)

--- dao/updates.go
+++ dao/updates.go
@@ -70,9 +70,9 @@ func UpdateDatabase() {
 		CreateDefaultOrganisation(ctx)
 		Updates.Insert(ctx, "create_default_organisation")
 	}
-	if !Updates.Check(ctx, "update_deposit_units_3") {
+	if !Updates.Check(ctx, "update_deposit_units_4") {
 		UpdateDepositUnitNorms(ctx)
-		Updates.Insert(ctx, "update_deposit_units_3")
+		Updates.Insert(ctx, "update_deposit_units_4")
 	}
 	if !Updates.Check(ctx, "publish_roles_initial") {
 		log.Print("publish_roles_initial")
