150a0ba0231c1477f920f8513d992649543154fa
Dennis Kleber <dennis_kleber@mailbox.org>
2025-09-08 12:17:49 +0200

    changes

--- dao/event.go
+++ dao/event.go
@@ -158,9 +158,6 @@ func EventsGetReceiverEvents(ctx context.Context, i *models.EventQuery, token *m
 func EventGetAps(ctx context.Context, i *models.EventQuery, token *models.AccessToken) (result *[]models.ListDetailsEvent, list_size int64, err error) {
 	filter := i.FilterAsp(token)
 	result = new([]models.ListDetailsEvent)
-	if err = EventCollection.Aggregate(ctx, models.EventPipeline(token).Match(filter).Pipe, result); err != nil {
-		return
-	}
 	sort := i.Sort()
 	pipeline := models.EventPipeline(token).SortFields(sort).Match(filter).Sort(sort).Skip(i.Skip, 0).Limit(i.Limit, 100).Pipe
 	cTx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
@@ -170,7 +167,6 @@ func EventGetAps(ctx context.Context, i *models.EventQuery, token *models.Access
 	}
 	count := vmod.Count{}
 	var cErr error
-
 	ctx2, cancel2 := context.WithTimeout(context.Background(), 60*time.Second)
 	defer cancel2()
 	if cErr = EventCollection.AggregateOne(ctx2, models.EventPipelinePublic().Match(filter).Count().Pipe, &count); cErr != nil {
