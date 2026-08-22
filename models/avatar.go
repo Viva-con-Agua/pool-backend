bf88fe86efe6d5afbafce57c1f92f4032e7a77b1
Eike Broda <git@ebroda.de>
2026-08-22 02:54:28 +0200

    Fix wrong filter of Avatar which disallowed delete of avatar
    As the avatar is now stored directly in the user table, the _id is
    the id of the user, no longer of the avatar object in its own collection.

--- models/avatar.go
+++ models/avatar.go
@@ -57,8 +57,7 @@ func (i *AvatarUpdate) PermittedFilter(token *AccessToken) bson.D {
 
 func (i *AvatarParam) PermittedFilter(token *AccessToken) bson.D {
 	filter := vmdb.NewFilter()
-	filter.EqualString("_id", i.ID)
-	filter.EqualString("user_id", token.ID)
+	filter.EqualString("_id", token.ID)
 	return filter.Bson()
 }
 
